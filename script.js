d71e3edaa9d17383a3841f9e1b284b6a4ffd4ec0
kkktonio <33447248+kkktonio@users.noreply.github.com>
2018-05-24 19:33:09 +0200

    tuyaux bougent + de tailles aléatoires
    rajouter des tuyaux en bas + les colisions

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function saut() {
-$('#oiseau').animate({marginBottom: '+=100px'},500);
+ $('#oiseau').animate({marginBottom: '+=100px'},500);
 }
 
 $(document).keydown(function (e){
-if (e.keyCode == 40){
+ if (e.keyCode == 40){
 //alert(e.keyCode);
-$('#oiseau').animate({marginTop: '+=50px'},100);
+ $('#oiseau').animate({marginTop: '+=50px'},100);
 }   //fleche du bas
-else if (e.keyCode == 38){
+ else if (e.keyCode == 38){
   $('#oiseau').animate({marginTop: '-=50px'},100);
 }   //fleche du haut
-else if (e.keyCode == 39){
+ else if (e.keyCode == 39){
   $('#oiseau').animate({marginLeft: '+=50px'},20);
 }  //fleche droite
 else if (e.keyCode == 37){
@@ -18,22 +18,50 @@ else if (e.keyCode == 37){
  }  //fleche gauche
 });
 
-setInterval(function (e){
- var p = $('#tuyau');
- var position = p.position();
- console.log(position);
- p.css({left: position.left-0.3},10);
-});
+    /**********façon différente de faire bouger un tuyau*********/
+
+// setInterval(function (e){
+//  var p = $('#tuyau1');
+//  var position = p.position();
+//  console.log(position);
+//  p.css({left: position.left-0.3},10);
+// });
+
+   /*******faire bouger tuyau + en faire défiler plusieurs*******/
+
+var n = 0;
+
+function generateObstacle(){
+
+n = n +1;
+    $('#section').append('<img src="https://image.ibb.co/iyJjRm/pipe.png"  class="obstacle" id="'+n+'">');
+    $('#'+n).animate({right: '+500px'},5000,"linear");
+
+    var taille = (Math.random()*13)-20;
+    var taille = Math.round(taille);
+      console.log(taille);
+
+ $('#'+n).css('top', taille + 'em');
+
+    function index(){
+      return (n + n-1);
+    }
+n = index(n);
+console.log(n);
+}
+
+setInterval(function(){generateObstacle();}, 2000);
+
+        /********Tuyaux en haut et en bas********/
+
+//
+// if (n+1 = ) {
+//  $()
+//
+// }
+
+        /****** Randomiser les tuyaux******/
 
 
-//function genererObstacle() {
-//}
-
-
-
-
-//function a_tuyau() {
-  //  $('#tuyau').animate({right: '+=400px'},3000,'linear');
-//}
 
 //console.log() pour voir ce qui est à l'intérieur de quoi
